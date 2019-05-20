1681cc680ccb2595f6d2882c7091ac528eded236
AVTahion <avkontakt40@gmail.com>
2019-05-20 23:34:45 +0300

    Tasks 1, 2

---
+++ GB_CSharp_lvl_2.5/DBClass.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GB_CSharp_lvl_2._5
+{
+    class DBClass
+    {
+        public List<Employee> employees = new List<Employee>();
+        public List<Department> departments = new List<Department>();
+
+        Random rnd = new Random();
+
+        public void Init(int x, int y)
+        {
+            for (int i = 0; i < y; i++)
+            {
+                DepAdd(new Department("department " + i));
+            }
+
+            for (int i = 0; i < x; i++)
+            {
+                EmpAdd(new Employee("fio " + i, rnd.Next(20, 60), departments[rnd.Next(0, y)].Name , rnd.Next(5000, 25000)));
+            }
+        }
+
+        public void EmpAdd(Employee emp)
+        {
+            employees.Add(emp);
+        }
+
+        public void DepAdd(Department dep)
+        {
+            departments.Add(dep);
+        }
+    }
+
+class Employee
+    {
+        public string FIO { get; set; }
+        public int Age { get; set; }
+        public string Department { get; set; }
+        public int Salary { get; set; }
+
+        public Employee(string fio, int age, string dep, int salary)
+        {
+            FIO = fio;
+            Age = age;
+            Department = dep;
+            Salary = salary;
+        }
+
+        public override string ToString()
+        {
+            return FIO;
+        }
+
+        public string DetailsToString()
+        {
+            return $"ФИО: {FIO}/n Возраст {Age}/n Департамент: {Department}/n Зарплата: {Salary}";
+        }
+    }
+
+    class Department
+    {
+        public string Name { get; set; }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
--- GB_CSharp_lvl_2.5/MainWindow.xaml.cs
+++ GB_CSharp_lvl_2.5/MainWindow.xaml.cs
@@ -13,6 +13,15 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+/*  1. Создать сущности Employee и Department и заполнить списки сущностей начальными данными.
+    2. Для списка сотрудников и списка департаментов предусмотреть визуализацию(отображение). Это можно сделать, например, с использованием ComboBox или ListView.
+    3. Предусмотреть редактирование сотрудников и департаментов.Должна быть возможность изменить департамент у сотрудника.Список департаментов для выбора можно выводить в ComboBox,
+        и все это можно выводить на дополнительной форме.
+    4. Предусмотреть возможность создания новых сотрудников и департаментов.Реализовать это либо на форме редактирования, либо сделать новую форму.
+
+    Александр Кушмилов
+*/
+
 namespace GB_CSharp_lvl_2._5
 {
     /// <summary>
@@ -20,9 +29,15 @@ namespace GB_CSharp_lvl_2._5
     /// </summary>
     public partial class MainWindow : Window
     {
+        DBClass db = new DBClass();
+
         public MainWindow()
         {
             InitializeComponent();
+            db.Init(10, 5);
+            listOfEmp.ItemsSource = db.employees;
+            listOfDep.ItemsSource = db.departments;
+
         }
     }
 }
